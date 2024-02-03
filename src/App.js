a2703eb827a93cf79ea8ee7ac12f3e5eb4357b3d
siyagampawar <115725393+siyagampawar@users.noreply.github.com>
2024-02-03 20:29:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import React from 'react';
+//import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+import './App.css';
+// import { FaBeer } from 'react-icons/fa'
+
+function App() {
+  return (
+    <div className="todoapp">
+     <TodoList />
+     {/* <FaBeer sixe = "3rem" /> */}
+    </div>
+  );
+}
+
+export default App;
